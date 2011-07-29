03323eacf9e9679ec243e7f99adc4f0cd60860ce
Vivien Barousse <barousse.vivien@gmail.com>
2011-07-29 16:05:04 +0100

    Name of new files is now encoded in URL

--- src/main/java/com/aperigeek/dropvault/service/AbstractFilesService.java
+++ src/main/java/com/aperigeek/dropvault/service/AbstractFilesService.java
@@ -26,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -169,7 +170,8 @@ public abstract class AbstractFilesService implements FilesService {
         }
         
         for (File createdFile : created) {
-            String href = dbFolder.getHref() + "/" + createdFile.getName();
+            String href = dbFolder.getHref() + "/" + 
+                    URLEncoder.encode(createdFile.getName(), "UTF-8");
             if (createdFile.isDirectory()) {
                 try {
                     client.mkcol(href);
